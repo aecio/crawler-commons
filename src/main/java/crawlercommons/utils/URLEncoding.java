8196cbe9960da4a837e20d8c17722f94278029df
Aécio Santos <aecio.solando@gmail.com>
2021-01-11 22:42:01 -0500

    Handle non-standard percent encoding (fix #135)

--- src/main/java/crawlercommons/utils/URLEncoding.java
+++ src/main/java/crawlercommons/utils/URLEncoding.java
@@ -81,16 +81,43 @@ public class URLEncoding {
         while (cb.hasRemaining()) {
             final char c = cb.get();
             if (c == '%' && cb.remaining() >= 2) {
-                final char uc = cb.get();
-                final char lc = cb.get();
-                final int u = Character.digit(uc, RADIX);
-                final int l = Character.digit(lc, RADIX);
-                if (u != -1 && l != -1) {
-                    bb.put((byte) ((u << 4) + l));
+                final char c1 = cb.get();
+                final char c2 = cb.get();
+                final int i1 = Character.digit(c1, RADIX);
+                final int i2 = Character.digit(c2, RADIX);
+
+                if (c1 == 'u' && cb.remaining() >= 3) {
+                    // Try to decode the UTF-16 non-standard %-encoding, i.e., the pattern
+                    // %uxxxx where xxxx is a UTF-16 code unit represented as four hexadecimal
+                    // digits. For more details see:
+                    //  - crawler-commons's issue #135
+                    //  - https://en.wikipedia.org/wiki/Percent-encoding#Non-standard_implementations
+                    final char c3 = cb.get();
+                    final char c4 = cb.get();
+                    final char c5 = cb.get();
+                    final int i3 = Character.digit(c3, RADIX);
+                    final int i4 = Character.digit(c4, RADIX);
+                    final int i5 = Character.digit(c5, RADIX);
+
+                    if (i2 != -1 && i3 != -1 && i4 != -1 && i5 != -1) {
+                        // all four chars are valid hex digits, decode the code point
+                        int codePoint = (i2 << 12) + (i3 << 8) + (i4 << 4) + i5;
+                        writeCodePoint(bb, codePoint);
+                    } else {
+                        // can't decode the xxxx sequence, rewind the last 4 chars
+                        cb.position(cb.position() - 4);
+                        bb.put((byte) '%');
+                        bb.put((byte) 'u');
+                    }
                 } else {
-                    bb.put((byte) '%');
-                    bb.put((byte) uc);
-                    bb.put((byte) lc);
+                    if (i1 != -1 && i2 != -1) {
+                        // decode standard percent-encoding
+                        bb.put((byte) ((i1 << 4) + i2));
+                    } else {
+                        bb.put((byte) '%');
+                        bb.put((byte) c1);
+                        bb.put((byte) c2);
+                    }
                 }
             } else if (plusAsBlank && c == '+') {
                 bb.put((byte) ' ');
@@ -100,6 +127,43 @@ public class URLEncoding {
         }
         bb.flip();
         return charset.decode(bb).toString();
+    }
+
+    /**
+     * Writes the Unicode code point {@code cp} into the given ByteBuffer
+     * {@code bb} using UTF-8 encoding.
+     *
+     * @param bb the byte buffer to write into
+     * @param cp the unicode code point
+     */
+    private static void writeCodePoint(ByteBuffer bb, int cp) {
+        final byte[] cpBytes = new byte[6];
+        if (cp < 0) {
+            throw new IllegalStateException("Negative code points are not allowed");
+        } else if (cp < 0x80) {
+            bb.put((byte) cp);
+        } else {
+            int bi = 0;
+            int lastPrefix = 0xC0;
+            int lastMask = 0x1F;
+            for (; ; ) {
+                int b = 0x80 | (cp & 0x3F);
+                cpBytes[bi] = (byte) b;
+                ++bi;
+                cp >>= 6;
+                if ((cp & ~lastMask) == 0) {
+                    cpBytes[bi] = (byte) (lastPrefix | cp);
+                    ++bi;
+                    break;
+                }
+                lastPrefix = 0x80 | (lastPrefix >> 1);
+                lastMask >>= 1;
+            }
+            while (bi > 0) {
+                --bi;
+                bb.put(cpBytes[bi]);
+            }
+        }
     }
 
     public static String encode(String name, Charset charset) {
